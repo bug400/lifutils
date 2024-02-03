1e1a5776f2c3229b66630d4b863b9c215d8d6f80
bug400 <bug400@gmx.de>
2024-02-03 15:36:05 +0100

    Fix

--- windows/run_nsis.cmd
+++ windows/run_nsis.cmd
@@ -2,5 +2,5 @@ set /p version=<..\version.txt
 if "%Platform%" EQU "x86" (
    "%NSISDIR%\makensis.exe" -NOCD -DINPDIR="install32" -DVERSTR=%version% ..\nsis\Setup.nsi
 ) else (
-   "%NSISDIR\makensis.exe" -NOCD -DINPDIR64="install64" -DVERSTR=%version% ..\nsis\Setup.nsi
+   "%NSISDIR%\makensis.exe" -NOCD -DINPDIR64="install64" -DVERSTR=%version% ..\nsis\Setup.nsi
 )
